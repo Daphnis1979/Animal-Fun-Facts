5ed86f8c88f1022bfc831acc41cd894709cf7284
Daphnis1979 <32830052+Daphnis1979@users.noreply.github.com>
2025-06-22 13:37:47 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import { animals } from "./animals";
 
 const title = "";
-const background = (
-  <img className="background" alt="ocean" src="/images/ocean.jpg" />
-);
-
 const images = [];
 
+function displayFact(e) {
+  const selectedAnimal = animals[e.target.alt];
+  const animalInfo = selectedAnimal.facts;
+
+  const optionIndex = Math.floor(Math.random() * animalInfo.length);
+  const funFact = animalInfo[optionIndex];
+
+  document.getElementById("fact").innerHTML=funFact;
+}
 for (const animal in animals) {
   images.push(
     <img
@@ -24,22 +28,19 @@ for (const animal in animals) {
   );
 }
 
-function displayFact(e) {
-  const selectedAnimal = animals[e.target.alt];
-  const optionIndex = Math.floor(Math.random() * selectedAnimal.facts.length);
-  const funFact = selectedAnimal.facts[optionIndex];
-  document.getElementById("fact").innerHTML = funFact;
-}
-
+const background = (
+  <img src="/images/ocean.jpg" alt="ocean" className="background" />
+);
 const animalFacts = (
   <div>
-    <h1>{title === "" ? "Click an Animal for a fun Fact" : title}</h1>
+    <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
     {background}
     <div className="animals">{images}</div>
     <p id="fact"></p>
   </div>
 );
-const container = document.getElementById("app");
 
+const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(animalFacts);
+
